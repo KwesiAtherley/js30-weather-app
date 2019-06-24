e24fe693ff4233002e3e497f197a77056beef871
kwesiatherley <kwesiatherley@gmail.com>
2019-06-24 16:42:52 -0400

    Corrected server misloading error

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-if (process.env.NODE.ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
 const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY
+const axios = require('axios')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
-app.use(express.static('publc'))
+app.use(express.static('public'))
 
 app.post('./weather', (req, res) => {
-
+    console.log(req.body)
 })
 
 app.listen(3000, () => {
