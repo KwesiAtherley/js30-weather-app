bdcbee2dce2679bfd5c673dd9869f9404a9283ef
kwesiatherley <kwesiatherley@gmail.com>
2019-06-26 14:19:33 -0400

    Styles Updates

--- public/script.js
+++ public/script.js
@@ -20,12 +20,14 @@ searchBox.addListener('places_changed', () => {
     })
 })
 
-
+const icon = new Skycons({ color: '#222' })
 const locationElement = document.querySelector('[data-location]')
 const statusElement = document.querySelector('[data-status]')
 const temperatureElement = document.querySelector('[data-temperature]')
 const precipitationElement = document.querySelector('[data-precipitation]')
 const windElement = document.querySelector('[data-wind]')
+icon.set('icon', 'clear-day')
+icon.play()
 
 function setWeatherData(data, place) {
     locationElement.textContent = place
@@ -33,4 +35,6 @@ function setWeatherData(data, place) {
     temperatureElement.textContent = data.temperature
     precipitationElement.textContent = `$(data.precipProbability * 100)%`
     windElement.textContent = data.windSpeed
+    icon.set('icon', data.icon)
+    icon.play()
 }
